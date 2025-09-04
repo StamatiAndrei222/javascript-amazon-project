b3515056f3a3acaab7dbd7ae2ae3000fff9b7fb3
Stamati Andrei <stamati@MacBook-Air-Stamati.local>
2025-09-04 12:33:29 +0300

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,15 @@
+import { loadProducts } from '../data/products.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-class.js';
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
+import { loadProducts } from '../data/products.js';
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+//a function without name
+loadProducts(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
