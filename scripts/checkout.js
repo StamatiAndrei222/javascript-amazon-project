b81f824d1f545e4491d7424e2e5316ddf9a6a20a
Stamati Andrei <stamati@MacBook-Air-Stamati.local>
2025-09-05 17:38:26 +0300

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,12 +4,58 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
-import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-//a function without name
-loadProducts(() => {
+Promise.all([
+    //resolve a function that lets us control when to go to the next step
+    new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
 
+/*
+//resolve a function that lets us control when to go to the next step
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+  
+}).then(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+//a function without name
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
